d25b53b11b336e994fc3f696432e929aabf3795c
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-30 10:52:38 +0200

    my decoder same as built-in

--- Part3/Convolutional_Codes/Convolutional.m
+++ Part3/Convolutional_Codes/Convolutional.m
@@ -1,5 +1,5 @@
 %% Generate bits to be tarnsmitted through channel
-Bits  = [0 0 1 0 1 1 0 1 0 1];
+Bits  = GenerateBits(1e5);
 
 %% Encode Bits
 RegLength = 3;             % Register length
@@ -8,9 +8,14 @@ GenPoly = [1 1 1; 0 1 1];      % Generator Polynomial [7; 3]
 EncodedBits = convEncode(Bits, RegLength, GenPoly);
 
 %% Decode
+% Built-in decoder
 myTrellis = poly2trellis(RegLength, [7 3]);
-decodedBits = viterbiAlgorithm(EncodedBits, RegLength, [7 3]);
+defDecodedBits = vitdec(EncodedBits,myTrellis,length(Bits),'trunc','hard');
 
-%% Compare sent with received
-sum(abs(Bits - decodedBits))
+% My decoder
+myDecodedBits = viterbiDecode(EncodedBits);
+
+
+%% Compare Built-in with my decoder
+sum(abs(myDecodedBits - defDecodedBits))
 

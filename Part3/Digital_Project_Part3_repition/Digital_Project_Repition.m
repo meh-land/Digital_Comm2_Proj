e75ca804463d0ea5a5646e5c85a845049cdac79f
Ahmed Aly El-Ghannam <ahmedaly.g112@gmail.com>
2023-12-30 23:32:14 +0200

    Added sweep for Pe in BSC

--- Part3/Digital_Project_Part3_repition/Digital_Project_Repition.m
+++ Part3/Digital_Project_Part3_repition/Digital_Project_Repition.m
@@ -7,26 +7,30 @@ close all;
 
 %% Define Essential Parameters
 % Total number of bits
-N_bits = 50; 
+N_bits = 5; 
 
 % Channel parameter (probability of bit flipping)
-p = 0.2;   
+p = 0:0.1:0.5;   
 
 % Sampling frequency
 fs = 10;
 
+% BER
+BER = [];
+
 %% Generate a bit sequence
-bit_seq = GenerateBits(N_bits)
+bit_seq = GenerateBits(N_bits);
 
 %% Generate Samples
 sample_seq = GenerateSamples(bit_seq,fs);
 
-%% Sampled binary sequence pass through BSC
-rec_sample_seq  = BSC(sample_seq,fs,p);
-
-%% Decoding the received samples
-rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,fs)
-
-
-
-
+%% Encoding && Decoding
+for i=1:1:5
+    % Sampled binary sequence pass through BSC @ different P's
+    rec_sample_seq = BSC(sample_seq,fs,p(i));
+    % Decoding the received samples @ different P's
+    rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,fs);
+    % Calculating BER
+    temp = ComputeBER(bit_seq,rec_bit_seq);
+    BER = [BER temp];
+end

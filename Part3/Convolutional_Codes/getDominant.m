447a0cf50a761e35f2ead6213f313ab50b805867
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-30 10:49:08 +0200

    getDominant returns pred

--- Part3/Convolutional_Codes/getDominant.m
+++ Part3/Convolutional_Codes/getDominant.m
@@ -1,15 +1,20 @@
-function dominantValue = getDominant(nodeValue)
+function [pred, dominantValue]= getDominant(nodeValue)
     nodeR = real(nodeValue);
     nodeI = imag(nodeValue);
     if nodeR < 0 && nodeI < 0
         dominantValue = nodeValue;
+        pred = 0;
     elseif nodeR >= 0 && nodeI < 0
         dominantValue = nodeR;
+        pred = 1;
     elseif nodeR < 0 && nodeI >= 0
         dominantValue = nodeI * 1i;
+        pred = 2;
     elseif nodeR >= 0 && nodeI >= 0 && nodeR < nodeI
         dominantValue = nodeR;
+        pred = 1;
     elseif nodeR >= 0 && nodeI >= 0 && nodeI < nodeR
         dominantValue = nodeI * 1i;
+        pred = 2;
     end
 end

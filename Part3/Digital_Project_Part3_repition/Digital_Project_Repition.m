e963df84953fa8706b1a41dafb5d6ff0eed57451
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-31 14:19:22 +0200

    fixed repetition v2

--- Part3/Digital_Project_Part3_repition/Digital_Project_Repition.m
+++ Part3/Digital_Project_Part3_repition/Digital_Project_Repition.m
@@ -7,10 +7,10 @@ close all;
 
 %% Define Essential Parameters
 % Total number of bits
-N_bits = 1000; 
+N_bits = 10; 
 
 % Channel parameter (probability of bit flipping)
-p = 0.01:0.01:0.5;   
+p = 0.1:0.1:0.5;   
 
 % Sampling frequency
 fs = 50;
@@ -26,7 +26,7 @@ bit_seq = GenerateBits(N_bits);
 sample_seq = GenerateSamples(bit_seq,fs);
 
 %% Encoding && Decoding (BSC)
-for i=1:1:50
+for i=1:1:5
     % Sampled binary sequence pass through BSC @ different P's
     rec_sample_seq = BSC(sample_seq,fs,p(i));
     % Decoding the received samples @ different P's
@@ -37,20 +37,23 @@ for i=1:1:50
 end
 
 %% Encoding && Decoding (BEC)
-for i=1:1:50
+for i=1:1:5
     % Sampled binary sequence pass through BEC @ different P's
     rec_sample_seq = BEC(sample_seq,fs,p(i));
     % Decoding the received samples @ different P's
     rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,fs);
     % Calculating BER
-    temp = ComputeBER(bit_seq,rec_bit_seq);
+    temp = sum(rec_bit_seq ~= bit_seq) / N_bits;
+    %temp = ComputeBER(bit_seq,rec_bit_seq);
     BER_BEC = [BER_BEC temp];
 end
 
+%% Plotting BER vs P (BSC)
 figure
 plot(p, BER_BSC, 'linewidth', 2)
 grid on
 
+%% Plotting BER vs P (BEC)
 figure
 plot(p, BER_BEC, 'linewidth', 2)
 grid on

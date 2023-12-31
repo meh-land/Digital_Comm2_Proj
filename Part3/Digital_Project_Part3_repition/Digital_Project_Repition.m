259f7af4faa07d6275d9c0b0da3bc7518ded7e68
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-31 14:01:51 +0200

    fixed repetition

--- Part3/Digital_Project_Part3_repition/Digital_Project_Repition.m
+++ Part3/Digital_Project_Part3_repition/Digital_Project_Repition.m
@@ -7,13 +7,13 @@ close all;
 
 %% Define Essential Parameters
 % Total number of bits
-N_bits = 5; 
+N_bits = 1000; 
 
 % Channel parameter (probability of bit flipping)
-p = 0:0.05:0.5;   
+p = 0.01:0.01:0.5;   
 
 % Sampling frequency
-fs = 10;
+fs = 50;
 
 % BER
 BER_BSC = [];
@@ -26,7 +26,7 @@ bit_seq = GenerateBits(N_bits);
 sample_seq = GenerateSamples(bit_seq,fs);
 
 %% Encoding && Decoding (BSC)
-for i=1:1:10
+for i=1:1:50
     % Sampled binary sequence pass through BSC @ different P's
     rec_sample_seq = BSC(sample_seq,fs,p(i));
     % Decoding the received samples @ different P's
@@ -35,9 +35,10 @@ for i=1:1:10
     temp = ComputeBER(bit_seq,rec_bit_seq);
     BER_BSC = [BER_BSC temp];
 end
+
 %% Encoding && Decoding (BEC)
-for i=1:1:10
-    % Sampled binary sequence pass through BSC @ different P's
+for i=1:1:50
+    % Sampled binary sequence pass through BEC @ different P's
     rec_sample_seq = BEC(sample_seq,fs,p(i));
     % Decoding the received samples @ different P's
     rec_bit_seq = DecodeBitsFromSamples(rec_sample_seq,fs);
@@ -45,3 +46,11 @@ for i=1:1:10
     temp = ComputeBER(bit_seq,rec_bit_seq);
     BER_BEC = [BER_BEC temp];
 end
+
+figure
+plot(p, BER_BSC, 'linewidth', 2)
+grid on
+
+figure
+plot(p, BER_BEC, 'linewidth', 2)
+grid on

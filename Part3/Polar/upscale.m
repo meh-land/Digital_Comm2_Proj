e90b85a44ed483cfc310fa412cd659f2224fb9fb
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-31 02:05:00 +0200

    graph BER vs P for polar

--- Part3/Polar/upscale.m
+++ Part3/Polar/upscale.m
@@ -20,15 +20,31 @@ encoded = [];
 for i = 1:K:Nb
     encoded = [encoded encoder(input(i:i+K-1),N,K)];
 end
-EbNo = 4;
-bit2symbol = 1 - 2 * encoded; 
+EbNo = 0:10;
+bit2symbol = 1 - 2 * encoded;
+BER = zeros(1, length(EbNo));
+p = zeros(1, length(EbNo));
+for EbNoIndex = 1:length(EbNo)
+    %% Add noise to samples
+    trans = awgn(bit2symbol,EbNo(EbNoIndex));
+    %% Compare bits before channel and after to get p (Probability of flipping)
+    binSymbolsBeforeChannel = bit2symbol > 0;
+    binSymbolsAfterChannel = trans > 0;
+    % Get probability of flipping for this EbNo
+    p(EbNoIndex) = sum(xor(binSymbolsBeforeChannel, binSymbolsAfterChannel))/length(trans);
+    %% Decoding received symbols
+    decoded = [];
+    for i = 1:N:Nb*2
+        decoded = [decoded decoder(trans(i:i+N-1),N,K)];
+    end
 
-%% Decoding the received samples
-trans = awgn(bit2symbol,EbNo);
-decoded = [];
-for i = 1:N:Nb*2
-    decoded = [decoded decoder(trans(i:i+N-1),N,K)];
+    %% Calculating BER
+    BER(EbNoIndex) = sum(decoded ~= input) / Nb;
 end
 
-%% Calculating BER
-BER = sum(decoded ~= input) / Nb
+%% Plot BER vs P
+figure
+plot(p, BER, 'linewidth', 2)
+xlabel('Probability of flipping')
+ylabel('BER')
+grid on
